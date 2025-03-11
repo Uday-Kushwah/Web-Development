6153ead68a2361ac2bec0f7954bb2e69763e4792
Uday Kushwah <165456051+Uday-Kushwah@users.noreply.github.com>
2025-03-11 14:56:46 +0530

    JS File

---
+++ Lecture_JS_18.js/2_SetTimeOut/setTimeOut.js
@@ -0,0 +1,17 @@
+/* 
+setTimeout(function() {
+
+}, milliseconds)
+*/
+
+// 5 seconds ka delay browser per daal diya, 
+// jab 5 seconds poore honge tab function ko call kr dega
+setTimeout(function(){
+    console.log("Hello World")
+}, 5000);
+
+
+console.log("Hiii.........")
+console.log("Kya Haal Chaal hai bhai")
+console.log("Aur kya ho rha hai")
+console.log("Free ho, fest mai chale ghumne")
